d1f5b1421862739075e12e5d30342e4423d64f83
Clinton Wong <clint.isaac.wong@gmail.com>
2013-10-21 09:31:57 -0600

    Added main driver file

---
+++ assignment2_main.rb
@@ -0,0 +1,8 @@
+# Main driver file for assignment 2.
+# Group 4
+# Members:
+# Jason Morawski
+# Tyler Schneider
+# Chris Beckett
+
+# Nothing in here yet, because we haven't implemented anything other than documentation and contracts.
