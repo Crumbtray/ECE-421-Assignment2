9274ccee9cc207ebc51dd8a521312012ebe1f30f
Clinton Wong <clint.isaac.wong@gmail.com>
2013-10-28 10:34:41 -0600

    Documentation and better output block

--- file_watcher_driver.rb
+++ file_watcher_driver.rb
@@ -3,6 +3,16 @@
 require './file_watcher'
 require 'fileutils'
 
-FileWatcher.FileWatch(10, ['ruby.txt'], 0) {puts "FACK"}
+# File Watcher usage:
+# FileWatcher.FileWatch(NMSEC, ListOfFiles, AlterationType) {BLOCK}
+# Where:
+# NMSEC - Number of milliseconds to delay
+# ListOfFiles - List of files to watch
+# AlterationType - Type of alteration to look for:
+# 	0 - Creation
+# 	1 - Modification
+# 	2 - Deletion
+# BLOCK - a block of actions to perform when the watcher activates
+FileWatcher.FileWatch(10, ['ruby.txt'], 0) {puts "File ruby.txt created."}
 FileUtils.touch('ruby.txt')
 FileUtils.rm('ruby.txt')
