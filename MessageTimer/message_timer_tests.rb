b6e18db9865eba41c659f696ee54a024f091a4bf
cbeckett <cbeckett@ualberta.ca>
2013-10-28 02:56:34 -0600

    Added partially implemented skeleton tests

---
+++ MessageTimer/message_timer_tests.rb
@@ -0,0 +1,21 @@
+require 'test/unit'
+require './message_timer'
+
+class TimerTests < Test::Unit::TestCase
+  def test_max_value
+    MessageTimer.timedMessage("999ms", "testMessage")
+  end
+  
+  def test_over_max_value
+    MessageTimer.timedMessage("1000ms", "testMessage")
+  end
+  
+  def test_under_min_value
+    #MessageTimer.timedMessage("-1s", "testMessage")
+  end
+  
+  def test_all_resolutions
+    #MessageTimer.timedMessage("1h1m1s1ms1ns", "testMessage")
+  end
+  
+end
