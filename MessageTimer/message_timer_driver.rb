8cc2ea36a4a84acccaefd09069720a10d3eae8e0
Clinton Wong <clint.isaac.wong@gmail.com>
2013-10-27 23:05:20 -0600

    Fixed driver output

--- MessageTimer/message_timer_driver.rb
+++ MessageTimer/message_timer_driver.rb
@@ -3,7 +3,9 @@ require './message_timer'
 
 
 if(ARGV.length != 2)
-	puts "Missing arguments. Try --help for more information."
+	puts "Missing arguments. Usage: message_timer_driver NMSEC MESSAGE"
+	puts "NMSEC - Time in milliseconds"
+	puts "MESSAGE - The message to display after NMSEC milliseconds"
 	exit 0
 end
 
