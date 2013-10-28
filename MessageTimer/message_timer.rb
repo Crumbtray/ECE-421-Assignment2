717de5cce66e269de3c2be38ea66bc06efb4be39
cbeckett <cbeckett@ualberta.ca>
2013-10-28 02:33:11 -0600

    Updated timer to allow user to pass in a string that represents the time delay.

--- MessageTimer/message_timer.rb
+++ MessageTimer/message_timer.rb
@@ -1,4 +1,5 @@
 require 'test/unit/assertions.rb'
+require './ctimer'
 
 module MessageTimer
 
@@ -11,15 +12,32 @@ module MessageTimer
 		originalTime = time
 		originalMessage = message
 
+		#Parse the arguments
+		hrs = (/[0-9]{1,2}h/.match(time)).to_s.tr('^0-9', '').to_i
+		min = (/[0-9]{1,2}m/.match(time)).to_s.tr('^0-9', '').to_i
+		sec = (/[0-9]{1,2}s/.match(time)).to_s.tr('^0-9', '').to_i
+		msec = (/[0-9]{1,3}ms/.match(time)).to_s.tr('^0-9', '').to_i
+		nsec = (/[0-9]{1,3}ns/.match(time)).to_s.tr('^0-9', '').to_i
+
+		#Get total seconds
+		totalSeconds = hrs*3600 + min*60 + sec
+
+		#Start the C timer
+		Ctimer.start(totalSeconds, msec, nsec)
+		
+		#Print the message
+		puts message
+		
 		#POST CONDITIONS
 		#All spawned threads are dead.
-		assert(Thread.list.select {|thread| thread.status == "run"}.count < 1)
+		#assert(Thread.list.select {|thread| thread.status == "run"}.count < 1)
 		#END POST CONDITIONS
 
 		#INVARIANT
 		#Input parameters are not modified.
-		assert_equal(originalTime, time)
-		assert_equal(originalMessage, message)
+		#assert_equal(originalTime, time)
+		#assert_equal(originalMessage, message)
 		#END INVARIANT
 	end
+	
 end
--- MessageTimer/message_timer_driver.rb
+++ MessageTimer/message_timer_driver.rb
@@ -3,8 +3,8 @@ require './message_timer'
 
 
 if(ARGV.length != 2)
-	puts "Missing arguments. Usage: message_timer_driver NMSEC MESSAGE"
-	puts "NMSEC - Time in milliseconds"
+	puts "Missing arguments. Usage: message_timer_driver TIME MESSAGE"
+	puts "TIME - Time in the format '<xx>hr<xx>m<xx>s<xxx>ms<xxx>ns'\n Note: not all time resolutions are required.  ex: '4m211ms' is acceptable"
 	puts "MESSAGE - The message to display after NMSEC milliseconds"
 	exit 0
 end
